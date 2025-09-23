a7e52e83f592fca58cea384db48ac2f67bc02ac9
Uyên Thy <uynthyee.313@gmail.com>
2025-09-23 14:27:09 +0700

    add top ui home

--- KitchenWhiz/src/pages/home/home.jsx
+++ KitchenWhiz/src/pages/home/home.jsx
@@ -1,14 +1,31 @@
 import { useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
-import {style} from './home.module.css';
+import style from './home.module.css';
+import corner from '/corner.png';
+import logo from '/logo.png';
 
 const Home = () => {
   const navigate = useNavigate();
     useEffect(() => {}, []);
     return ( 
-        <div className={style}>
-            <h1>Welcome to KitchenWhiz</h1>
-            <button onClick={() => navigate('/about')}>Go to About Page</button>
+        <div className={style.background}>
+            <div className = {style.top}>
+            <div className={style.bar}>
+                <img src={logo} alt="Logo" className={style.logo} onClick={() => navigate('/')} />
+                <div className={style.textbar}>Chào mừng, Uyên Thy</div>
+                <div className={style.avatarcontainer}>
+                    <img src={corner} alt="Avatar" className={style.avatar} />
+                </div>
+            </div>
+            <img src={corner} alt="Corner Decoration" className={style.imgcorner} />
+            <div className={style.imgcontainer}>
+                <img src={corner} alt="Corner Decoration" className={style.image} />
+            </div>
+            <div className={style.randomMeals}>
+                <div className={style.titleRandomMeals}>Trưa nay ăn gì?</div>
+                <div className={style.descriptionRandomMeals}>is a popular Vietnamese noodle soup originating from the city of Huế in central Vietnam. This dish features a rich and aromatic broth made from beef bones and...</div>
+            </div>
+            </div>
         </div>
     );
 
--- KitchenWhiz/src/router/routes.jsx
+++ KitchenWhiz/src/router/routes.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route, useLocation } from 'react-router-dom';
 import Login from '../pages/login/login';
 import Signup from '../pages/signup/signup';
+import Home from '../pages/home/home';
 const AppRoutes = () => {
   const location = useLocation();
   return (
     <Routes location={location} key={location.pathname}>
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="*" element={<Home />} />
     </Routes>
   );
 }
